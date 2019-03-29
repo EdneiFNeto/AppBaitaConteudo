00cfc2e9e9985b3b2dd17fd642878c247a7f7561
Ednei de Freitas Neto <ednei.freitas@moebius.com.br>
2019-03-29 08:48:38 -0300

    finsh app

--- app/src/main/java/com/example/appbaitaconteudo/MainActivity.java
+++ app/src/main/java/com/example/appbaitaconteudo/MainActivity.java
@@ -2,74 +2,174 @@ package com.example.appbaitaconteudo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnTouchListener {
 
-    VideoView videoView;
-    String TAG = "MainLog";
-    ListView listView;
-    RelativeLayout relativeLayout;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    String url = "http://189.45.13.225/stream.php.m3u8?user=baita&pass=2018tv&token=1553733132&resptime=109&s=stream29.m3u8";
-
-    WindowManager windowManager;
+    private VideoView videoView;
+    private String TAG = "MainLog";
+    private ListView listView;
+    private RelativeLayout relativeLayout;
+    private WindowManager windowManager;
     private int screenWidth, screenHeight;
+    private Toolbar myToolbar;
+    private ProgressBar spinner;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private boolean isladscape;
 
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoView.pause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         videoView = (VideoView) findViewById(R.id.videoview);
-        listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(new AdapterChannel(this));
-        listView.setOnItemClickListener(this);
 
-        relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout);
-        relativeLayout.setOnTouchListener(this);
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.inflateMenu(R.menu.menu);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.mipmap.ic_logo_2);
+
+        spinner = (ProgressBar) findViewById(R.id.progress_bar);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        //drawerLayout.setDrawerListener(mToggle);
+        //mToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        //detect size display
-        Display display = windowManager.getDefaultDisplay();
-        DisplayMetrics metrics = new DisplayMetrics();
-        display.getMetrics(metrics);
-
-        //setting size display
-        screenWidth = metrics.widthPixels;
-        screenHeight = metrics.heightPixels;
-
         try {
+            windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+            //detect size display
+            Display display = windowManager.getDefaultDisplay();
+            DisplayMetrics metrics = new DisplayMetrics();
+            display.getMetrics(metrics);
+
+            //setting size display
+            screenWidth = metrics.widthPixels;
+            screenHeight = metrics.heightPixels;
+
+            Configuration configuration = getResources().getConfiguration();
+
+            if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                myToolbar.setVisibility(View.GONE);
+                fullScreen();
+            } else {
+                myToolbar.setVisibility(View.VISIBLE);
+            }
+
             playVideo(0);
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             Log.e(TAG, e.getMessage());
         }
+
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        try{
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
-        playVideo(position);
-        }catch (Exception e){
-            Toast.makeText(this, "Error click", Toast.LENGTH_LONG).show();
+        try {
+            if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                myToolbar.setVisibility(View.GONE);
+                fullScreen();
+
+            } else {
+                showSystemUI();
+                myToolbar.setVisibility(View.VISIBLE);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Error changed orientation: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
         }
     }
+
+    public void fullScreen() {
+
+        try {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                    int uiOptions =
+                            View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                    View decorView = getWindow().getDecorView();
+                    decorView.setSystemUiVisibility(uiOptions);
+                }
+            }, 1000);
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Error fullscreen: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void showSystemUI() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+            }
+        }, 1000);
+    }
+
 
     private void playVideo(int position) {
 
@@ -95,6 +195,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case 5:
                 channel = url + "28.m3u8";
                 break;
+
             case 6:
                 channel = url + "27.m3u8";
                 break;
@@ -106,10 +207,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case 8:
                 channel = url + "35.m3u8";
                 break;
+
+            case 9:
+                channel = url + "42.m3u8";
+                break;
+
+            case 10:
+                channel = url + "37.m3u8";
+                break;
         }
 
         videoView.setVideoURI(Uri.parse(channel));
-        listView.setVisibility(View.GONE);
+        spinner.setVisibility(View.VISIBLE);
 
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
@@ -122,14 +231,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             @Override
             public void onPrepared(MediaPlayer mp) {
                 videoView.start();
+                spinner.setVisibility(View.GONE);
             }
         });
 
         videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
             public boolean onError(MediaPlayer mp, int what, int extra) {
-                Log.e(TAG, "Error: " + what);
-                return false;
+                spinner.setVisibility(View.GONE);
+                videoView.setBackgroundResource(R.drawable.ic_error_video);
+                return true;
             }
         });
 
@@ -137,35 +248,50 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.channel_0:
+                playVideo(0);
+                break;
+            case R.id.channel_1:
+                playVideo(1);
+                break;
+            case R.id.channel_2:
+                playVideo(2);
+                break;
+            case R.id.channel_3:
+                playVideo(3);
+                break;
+            case R.id.channel_4:
+                playVideo(4);
+                break;
+            case R.id.channel_5:
+                playVideo(5);
+                break;
 
-        try {
+            case R.id.channel_6:
+                playVideo(6);
+                break;
 
-            float x = event.getX();
-            float y = event.getY();
-            float width = screenWidth / 2;
-            float height = screenHeight;
+            case R.id.channel_7:
+                playVideo(7);
+                break;
 
-            switch (event.getAction()) {
+            case R.id.channel_8:
+                playVideo(8);
+                break;
 
-                case MotionEvent.ACTION_DOWN:
+            case R.id.channel_9:
+                playVideo(9);
+                break;
 
-                    if (((int) x >= 10 && (int) x <= (int) width)) {
-                        listView.setVisibility(View.VISIBLE);
-                    }
+            case R.id.channel_10:
+                playVideo(10);
+                break;
 
-                    if ((int) x > (int) width) {
-                        listView.setVisibility(View.GONE);
-                    }
-
-                    break;
-            }
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
-
-
+        //close on click item
+        drawerLayout.closeDrawer(GravityCompat.START);
         return false;
     }
 }
---
+++ app/src/main/java/com/example/appbaitaconteudo/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package com.example.appbaitaconteudo.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.appbaitaconteudo.R;
+import com.example.appbaitaconteudo.model.DataModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter {
+
+
+    ArrayList mValues;
+    Context mContext;
+    protected ItemListener mListener;
+
+    public RecyclerViewAdapter(Context context, ArrayList values, ItemListener itemListener) {
+
+        mValues = values;
+        mContext = context;
+        mListener=itemListener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.recycler_view_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public interface ItemListener {
+        void onItemClick(DataModel item);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView textView;
+        public ImageView imageView;
+        public RelativeLayout relativeLayout;
+        DataModel item;
+
+        public ViewHolder(View v) {
+
+            super(v);
+
+            v.setOnClickListener(this);
+            textView = (TextView) v.findViewById(R.id.textView);
+            imageView = (ImageView) v.findViewById(R.id.imageView);
+            relativeLayout = (RelativeLayout) v.findViewById(R.id.relativeLayout);
+
+        }
+
+        public void setData(DataModel item) {
+            this.item = item;
+
+            textView.setText(item.text);
+            imageView.setImageResource(item.drawable);
+            relativeLayout.setBackgroundColor(Color.parseColor(item.color));
+
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            if (mListener != null) {
+                mListener.onItemClick(item);
+            }
+        }
+    }
+}
